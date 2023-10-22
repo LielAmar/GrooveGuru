8514bb700fb3f09459760fb08dc692fb47bee8d4
Liel Amar <liel@lielamar.com>
2023-10-22 14:59:59 -0400

    Changed font

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,13 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Rubik:100,200,300,400,500,600,700,800&subset=latin,hebrew"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </head>
       <body>
         {children}
       </body>
