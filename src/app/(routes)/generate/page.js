9ed3f5a03580980ba9d5d1e0391647866555d087
Liel Amar <liel@lielamar.com>
2023-10-22 14:50:47 -0400

    Changed generate page label

--- src/app/(routes)/generate/page.js
+++ src/app/(routes)/generate/page.js
@@ -95,7 +95,7 @@ export default function Generate() {
     <main className="flex flex-row justify-center items-center">
       <div className="flex flex-col items-center">
         {/* TITLE */}
-        <h1 className="mt-5 text-white text-4xl font-bold">Your {MOODS[mood].name} playlist </h1>
+        <h1 className="mt-5 text-white text-4xl font-bold">Your {MOODS[mood].name} Playlist </h1>
 
         {/* PLAYLIST */}
         <div className="mt-10 grid grid-cols-3 gap-5">
