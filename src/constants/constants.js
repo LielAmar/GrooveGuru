209737070ad33fe8bd51d6f0d89fcf20fef60185
Liel Amar <liel@lielamar.com>
2023-11-01 13:32:07 -0400

    Added signer list

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -19,3 +19,26 @@ export const GENRES = [
   { name: "Reggae" },
   { name: "Folk" },
 ]
+
+export const ARTISTS = [
+  { name: "Taylor Swift" },
+  { name: "Kendrick Lamar" },
+  { name: "Alicia Keys" },
+  { name: "Bob Dylan" },
+  { name: "Ella Fitzgerald" },
+  { name: "Daft Punk" },
+  { name: "Johnny Cash" },
+  { name: "Kanye West" },
+  { name: "Miles Davis" },
+  { name: "Dolly Parton" },
+  { name: "Beyoncé" },
+  { name: "Frank Sinatra" },
+  { name: "Stevie Wonder" },
+  { name: "Aretha Franklin" },
+  { name: "Elvis Presley" },
+  { name: "Billie Holiday" },
+  { name: "The Beatles" },
+  { name: "Bruno Mars" },
+  { name: "Whitney Houston" },
+  { name: "Janis Joplin" },
+];
